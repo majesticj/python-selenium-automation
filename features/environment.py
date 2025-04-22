5d9557b8f3d720c3142f9ad2f232aae21dac479f
jay404077047@gmail.com <jeromemj4040@gmail.com>
2025-04-21 20:16:15 -0500

    lesson 6

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
-
+from app.application import Application
 
 def browser_init(context):
     """
@@ -15,6 +16,8 @@ def browser_init(context):
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
+    context.wait = WebDriverWait(context.driver, 10)
+    context.app = Application(context.driver)
 
 
 def before_scenario(context, scenario):
@@ -33,4 +36,17 @@ def after_step(context, step):
 
 def after_scenario(context, feature):
     context.driver.delete_all_cookies()
+    context.driver.quit()
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+def before_all(context):
+    options = Options()
+    options.add_argument("--start-maximized")
+    service = Service()
+    context.driver = webdriver.Chrome(service=service, options=options)
+
+def after_all(context):
     context.driver.quit()
